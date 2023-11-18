2aae239148d11349489a9c90bd5117c09abcde2d
howard-rose <philip.anthon@gmail.com>
2023-11-19 07:55:10 +0800

    silhouette score fixes

--- CSMODEL-Project-S11-Group2/kmedians.py
+++ CSMODEL-Project-S11-Group2/kmedians.py
@@ -1,5 +1,4 @@
 import math
-
 import numpy as np
 import pandas as pd
 from kmeans import KMeans
@@ -51,82 +50,6 @@ class KMedians(KMeans):
 
         return wcss
 
-    # def get_cluster_index_of_point(self, i, groups):
-    #     pass
-
-    '''
-    def __get_silhouette_template(self, data, groups, a, b):
-        def s(i):
-            cluster_I = self.zget_cluster_of_point(i)
-            if len()
-        pass
-
-
-
-
-    
-
-
-    def get_silhouette_score_simplified(self, data, groups):
-        def a(i):
-            return self.get_euclidean_distance()
-        def b(i):
-            pass
-
-        return (b_val - a_val) / max(a_val, b_val) if len(cluster_I) > 1 else 0
-    '''
-
-    def get_silhouette_score(self, data, groups, simplified=True):
-        def s(i):
-            nonlocal clusters
-            nonlocal I
-
-            # If cluster only contains 1 point, return 0
-            # a single point is represented by a Series
-            if type(clusters[I]) == pd.Series:
-                return 0
-
-            if simplified:
-                def a(i):
-                    nonlocal I
-                    return self.get_euclidean_distance(i, self.centroids.iloc[I])
-                def b(i):
-                    nonlocal I
-                    nearest_neighbor_distance = self.get_euclidean_distance(i, self.centroids.drop(index=I)).min()
-                    return nearest_neighbor_distance if not math.isnan(nearest_neighbor_distance) else math.inf
-            else:
-                def a(i):
-                    nonlocal clusters
-                    nonlocal I
-                    return self.get_euclidean_distance(i, clusters[I]).sum() / (len(clusters[I]) - 1)
-                def b(i):
-                    nonlocal clusters
-                    nonlocal I
-                    nearest_neighbor_distance = math.inf
-                    for J in range(self.k):
-                        if I != J:
-                            nearest_neighbor_distance = min(nearest_neighbor_distance, self.get_euclidean_distance(i, clusters[J]).sum() / len(clusters[J]))
-                    return nearest_neighbor_distance
-
-            a_val = a(i)
-            b_val = b(i)
-            return (b_val - a_val) / max(a_val, b_val)
-
-        # #grouped_data = pd.concat([data, groups.rename('group')], axis=1)
-        # for I in range(self.k):
-        #     cluster_I = grouped_data.loc[grouped_data['group'] == I]
-
-        clusters = [c for _, c in data.groupby(groups)]
-
-        total_score = 0
-        for I in range(self.k):
-            total_score += clusters[I].apply(s, axis=1).sum()
-
-        average_score = total_score / len(data)
-        print(f'Silhouette score with {self.k} clusters: {average_score}')
-        return average_score
-
-        #return grouped_data.apply(s).mean()
 
     def get_silhouette_score_simplified(self, data, groups):
         def s(i):
@@ -147,20 +70,6 @@ class KMedians(KMeans):
 
         grouped_data = pd.concat([data, groups.rename('group')], axis=1)
 
-        #average_score = grouped_data.apply(s, axis=1).mean()
-        average_score = 0
-        for _, i in grouped_data.iterrows():
-            I = i['group'].astype(np.int32)
-
-            # If cluster only contains 1 point, return 0
-            if cluster_counts.iloc[I] == 1:
-                return 0
-
-            a_val = self.get_euclidean_distance(i, self.centroids.iloc[I])
-            b_val = self.get_euclidean_distance(i, self.centroids.drop(index=I)).min()
-
-            average_score += (b_val - a_val) / max(a_val, b_val)
-        average_score /= len(data)
-        #average_score = np.vectorize(s)(grouped_data, grouped_data['group'].astype(np.int32))
+        average_score = grouped_data.apply(s, axis=1).mean()
         print(f'Silhouette score with {self.k} clusters: {average_score}')
         return average_score
